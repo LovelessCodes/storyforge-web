b21eaf17515c570d87bf02dd81154d265dbde741
Mike Kold Hermann <mkh@cleanmanager.dk>
2025-10-06 14:59:01 +0200

    style(utils): 🎨 format code for improved readability
    * Reorganized imports and type definitions for consistency.
    * Adjusted indentation and spacing in the `getPlatformFromAssetUrl` function.
    * Ensured proper formatting in the `DownloadInfo` type definition.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,23 +1,29 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
 
-export type PlatformKey = "darwin-aarch64" | "linux-x86_64" | "darwin-x86_64" | "linux-aarch64" | "windows-x86_64";
+export type PlatformKey =
+	| "darwin-aarch64"
+	| "linux-x86_64"
+	| "darwin-x86_64"
+	| "linux-aarch64"
+	| "windows-x86_64";
 
 export const getPlatformFromAssetUrl = (url: string): PlatformKey | null => {
-    if (url.endsWith("_aarch64.dmg")) return "darwin-aarch64";
-    if (url.endsWith("_amd64.AppImage")) return "linux-x86_64";
-    if (url.endsWith("_x64.dmg")) return "darwin-x86_64";
-    if (url.endsWith("_aarch64.AppImage")) return "linux-aarch64";
-    if (url.endsWith("_x64_en-US.msi")) return "windows-x86_64";
-    return null;
-}
+	if (url.endsWith("_aarch64.dmg")) return "darwin-aarch64";
+	if (url.endsWith("_amd64.AppImage")) return "linux-x86_64";
+	if (url.endsWith("_x64.dmg")) return "darwin-x86_64";
+	if (url.endsWith("_aarch64.AppImage")) return "linux-aarch64";
+	if (url.endsWith("_x64_en-US.msi")) return "windows-x86_64";
+	return null;
+};
 
 export type DownloadInfo = {
-    assets: {
-        browser_download_url: string;
-    }[]
-}
+	html_url: string;
+	assets: {
+		browser_download_url: string;
+	}[];
+};
